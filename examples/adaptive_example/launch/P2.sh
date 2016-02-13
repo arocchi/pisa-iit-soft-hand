884b76799985686c3c86f9cb63b542289cb9acf1
Alessio Rocchi <alessio.rocchi@iit.it>
2016-02-13 23:42:04 +0100

    Fixed P2.sh

--- examples/adaptive_example/launch/P2.sh
+++ examples/adaptive_example/launch/P2.sh
@@ -2,14 +2,14 @@
 ./open_hand.sh
 ./move_hand_P2.sh
 sleep 4
-./close_hand.sh 0.9
+./close_hand.sh 0.5
 sleep 5
 echo "Finished closing"
 rostopic echo -n 1 /clock > P2_timing.txt
 sleep 5
 echo "Starting lift"
 rostopic echo -n 1 /clock >> P2_timing.txt
-./move_hand_P1_up.sh
+./move_hand_P2_up.sh
 read
 echo "Saving time"
 rostopic echo -n 1 /clock >> P2_timing.txt
