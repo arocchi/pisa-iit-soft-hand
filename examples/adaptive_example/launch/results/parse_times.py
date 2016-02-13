de655b6998acaf05a5c731b0938b39d29a44db96
Alessio Rocchi <alessio.rocchi@iit.it>
2016-02-13 19:05:13 +0100

    Added ability to compute means in parse_times

--- examples/adaptive_example/launch/results/parse_times.py
+++ examples/adaptive_example/launch/results/parse_times.py
@@ -5,6 +5,15 @@ from scipy import signal as sig
 from matplotlib import pyplot as plt
 
 
+def parse_mean(t, t_min, t_max):
+    t_other = t[t[:, 1] < t_min]
+    t_contact = t[t[:, 1] >= t_min]
+    t_contact = t_contact[t_contact[:, 1] <= t_max]
+    t_contact_v = t_contact[1:, 0] - t_contact[:-1, 0]
+    t_other_v = t_other[1:, 0] - t_other[:-1, 0]
+    return (np.mean(t_contact_v), np.mean(t_other_v))
+
+
 def parse_t(f):
     reader = csv.reader(f, delimiter=" ")
     t = False
@@ -21,6 +30,13 @@ def parse_t(f):
 fn = sys.argv[1]
 f = open(fn, 'r')
 t = parse_t(f)
+
+if(len(sys.argv) == 4):
+    t_min = float(sys.argv[2])
+    t_max = float(sys.argv[3])
+    (mean_contact, mean_other) = parse_mean(t, t_min, t_max)
+    print "Mean of Processing Time during Contact:", mean_contact
+    print "Mean of Processing Time, not in Contact:", mean_other
 
 plt.plot(t[1:, 1], sig.medfilt(t[1:, 0] - t[:-1, 0], 11))
 plt.show()
