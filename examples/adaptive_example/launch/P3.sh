59350c18b19a5155f09e88d397e3b63797f3c0f5
Alessio Rocchi <alessio.rocchi@iit.it>
2016-02-14 22:50:45 +0100

    Slower closing makes grasping more stable

--- examples/adaptive_example/launch/P3.sh
+++ examples/adaptive_example/launch/P3.sh
@@ -1,9 +1,13 @@
 ./move_hand_P3_up.sh
 ./open_hand.sh
-./sleep.py 5
+./sleep.py 10
 ./move_hand_P3.sh
+./sleep.py 2
+./close_hand.sh 0.2
 ./sleep.py 1
 ./close_hand.sh 0.3
+./sleep.py 1
+./close_hand.sh 0.4
 ./sleep.py 3
 echo "Finished closing"
 rostopic echo -n 1 /clock > timing.txt
